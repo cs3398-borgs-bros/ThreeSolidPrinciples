6e8efea36700873bf9c935095d42a7d4f455a207
CarterK33 <C_k133@txstate.edu>
2019-02-18 19:40:27 -0600

    Add files via upload

--- src/threesolid/interface.java
+++ src/threesolid/interface.java
@@ -1,24 +1,13 @@
-/* 
-The open close principle apples to this becasue the
-SuperWorker class is open for extension and the original code is closed for modification 
-
-The single responsiblity principle applies to this because both the IWorkable and IFeedable both have a 
-single responsiblity or function. 
-
-
-The interface segregation principle applies to the IWorker interface by splittin it up into seperate interfaces IWorkable and Ifeedable. 
-This is more benefical because the original interface was polluted because of the two different types of workers and there different needs,
-the robot class doesnt need the eat function. Thus by using the interface segregation principle solves this. 
-
-*/
-
-
 package threesolid;
 
-interface IWorkable {
-	public void work();
+interface IEat {
+	public void eat();
 }
 
-interface IFeedable{
-	public void eat();
+interface ISick{
+	public void sick();
+}
+
+interface IReboot{
+	public void reboot(); 
 }
