52cab0dacb0b9cac935329f9c93e0a6e6d7331c9
dal79 <dal79@users.noreply.github.com>
2019-02-18 22:47:30 -0600

    Update Main.java

--- src/threesolid/Main.java
+++ src/threesolid/Main.java
@@ -3,38 +3,39 @@ package threesolid;
 
 public class Main
 {   
-  public static Manager testManager = new Manager();
-
-
+  public static Manager regManager = new Manager();
+  public static ProductManager prodManager = new ProductManager();
+  public static ProjectManager projManager = new ProjectManager();
   // The entry main() method
   public static void main(String[] args) 
   {
     System.out.format("Starting management ... \n"); 
+
     try 
     {  
-      System.out.format("Hiring new Worker.. \n"); 
-      testManager.setWorker(new Worker());
-      System.out.format("Managing Worker.. \n"); 
-      testManager.manage();
+      System.out.format("\nManager hiring new TempWorker.. \n"); 
+      regManager.setWorker(new TempWorker());
+      System.out.format("Managing TempWorker.. \n"); 
+      regManager.manage();
 
-      
-      System.out.format("Hiring new SuperWorker.. \n");
-      testManager.setWorker(new SuperWorker());
-      System.out.format("Managing SuperWorker.. \n");
-      testManager.manage();
+      System.out.format("\nProduct Manager hiring new SuperWorker.. \n"); 
+      prodManager.setWorker(new SuperWorker());
+      prodManager.defineproduct();
+      System.out.format("Managing SuperWorker.. \n"); 
+      prodManager.manage();
 
-
-      System.out.format("Hiring new Robot.. \n");
-      testManager.setWorker(new Robot());
+      System.out.format("\nProject Manager hiring new Robot.. \n");
+      projManager.setWorker(new Robot());
+      projManager.schedulework();
       System.out.format("Managing Robot.. \n");
-      testManager.manage();
+      projManager.manage();
     } 
     catch (Exception main_except)
     {
       main_except.printStackTrace();
     }
 
-    System.out.format("Management complete. \n"); 
+    System.out.format("\nManagement complete. \n"); 
     System.exit(0);
   }
 }
