b3ae7bfbfd0333b953b8c85dd2974819cb89ea00
dal79 <dal79@users.noreply.github.com>
2019-02-18 19:15:17 -0600

    Create BaseWorker.java
    This class to be used by all worker classes.

---
+++ src/threesolid/BaseWorker.java
@@ -0,0 +1,10 @@
+/*
+ * A class containing a work method for all worker classes.
+*/
+package threesolid;
+
+class BaseWorker implements IWorkable{
+	public void work() {
+		System.format("work work work...");
+	}
+}
